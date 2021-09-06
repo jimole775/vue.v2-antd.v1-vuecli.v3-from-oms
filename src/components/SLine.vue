31efd22864decd33219c5d86f7a310220138762e
16603013472 <rongchuanda@chinasie.com>
2021-09-06 14:59:24 +0800

    feat: line component update

--- src/components/SLine.vue
+++ src/components/SLine.vue
@@ -3,39 +3,41 @@ import utils from '@/utils'
 export default {
   functional: true,
   name: 'SLine',
-  render (h, data) {
-    const { value = '', len = 10, lineEnd = 'ellipsis', rows = 3 } = data.props || {}
+  render (h, vm) {
+    const { value = '', len = 10, lineEnd = 'ellipsis', rows = 3 } = vm.props || {}
+    const style = vm.data.style || {}
     if (!value) return ''
     let sentence = ''
     if (lineEnd === 'break') {
       sentence = utils.breakSentence(value, len, rows)
-    } else {
+    }
+    if (lineEnd === 'ellipsis') {
       sentence = utils.ellipsisSentence(value, len)
     }
     if (utils.isString(sentence)) {
       if (value === sentence) {
-        return <div>{ value }</div>
+        return <div class="line-standard" style={style}>{ value }</div>
       } else {
         return (
           <a-tooltip title={value}>
-            <div class="overHidden3">{ sentence }</div>
+            <div class="line-standard" style={style}>{ sentence }</div>
           </a-tooltip>
         )
       }
     } else if (utils.isArray(sentence)) {
       return (
         <a-tooltip title={value}>
-          <div class="overHidden3">
+          <div class="line-standard" style={style}>
             {
               sentence.map((line) => {
-                return <div>{ line }</div>
+                return <div class="line-standard">{ line }</div>
               })
             }
           </div>
         </a-tooltip>
       )
     } else {
-      return <div>{ value }</div>
+      return <div class="line-standard" style={style}>{ value }</div>
     }
   }
 }
--- src/components/SLines.vue
+++ src/components/SLines.vue
@@ -3,28 +3,33 @@ import utils from '@/utils'
 export default {
   functional: true,
   name: 'SLines',
-  render (h, data) {
-    const { len = 20, value = '', lineEnd = 'ellipsis' } = data.props || {}
+  render (h, vm) {
+    const { len = 20, value = '', lineEnd = 'ellipsis' } = vm.props || {}
+    const style = vm.data.style || {}
     const sentences = getSentences(value)
     return (
-      sentences.map((sentence) => {
-        if (lineEnd === 'ellipsis') {
-          // xxxxx..
-          // xxxxx..
-          return <SLine len={len} value={sentence} />
-        } else if (lineEnd === 'break') {
-          // xxxxxxx
-          // xxx
-          // xxxxxxx
-          // xxxxx
-          const lines = utils.breakSentence(sentence, len)
-          return lines.map(line => {
-            return <div>{ line }</div>
+      <div style={style}>
+        {
+          sentences.map((sentence) => {
+            if (lineEnd === 'ellipsis') {
+              // xxxxx..
+              // xxxxx..
+              return <SLine style={style} len={len} value={sentence} />
+            } else if (lineEnd === 'break') {
+              // xxxxxxx
+              // xxx
+              // xxxxxxx
+              // xxxxx
+              const lines = utils.breakSentence(sentence, len)
+              return lines.map(line => {
+                return <div class="line-standard">{ line }</div>
+              })
+            } else {
+              return ''
+            }
           })
-        } else {
-          return ''
         }
-      })
+      </div>
     )
   }
 }
